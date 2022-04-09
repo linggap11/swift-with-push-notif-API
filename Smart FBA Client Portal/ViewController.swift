683ccb6c935745af7abc3440947bd8e2eb098b5a
pangestur11@gmail.com <pangestur11@gmail.com>
2022-04-09 11:45:13 +0700

    Initial Commit

---
+++ Smart FBA Client Portal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart FBA Client Portal
+//
+//  Created by Lingga on 09/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
