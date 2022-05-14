ef28c492f712664d9051aa52645c97aa3e4e91a4
pangestur11@gmail.com <pangestur11@gmail.com>
2022-05-15 05:53:20 +0700

    first commit

--- Smart FBA Client Portal/AppDelegate.swift
+++ Smart FBA Client Portal/AppDelegate.swift
@@ -4,33 +4,228 @@
 //
 //  Created by Lingga on 09/04/22.
 //
-
 import UIKit
+import UserNotifications
+import FirebaseCore
+import FirebaseMessaging
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+//  var window: UIWindow?
+  let gcmMessageIDKey = "gcm.message_id"
 
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication
+                     .LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
 
+    // [START set_messaging_delegate]
+    Messaging.messaging().delegate = self
+    // [END set_messaging_delegate]
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    // Register for remote notifications. This shows a permission dialog on first run, to
+    // show the dialog at a more appropriate time move this registration accordingly.
+    // [START register_for_notifications]
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
+    } else {
+      let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
     }
 
-    // MARK: UISceneSession Lifecycle
+    application.registerForRemoteNotifications()
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    // [END register_for_notifications]
+
+    return true
+  }
+    
+
+  func application(_ application: UIApplication,
+                   didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+    // If you are receiving a notification message while your app is in the background,
+    // this callback will not be fired till the user taps on the notification launching the application.
+    // TODO: Handle data of notification
+
+    // With swizzling disabled you must let Messaging know about the message, for Analytics
+    // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+    // Print message ID.
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    // Print full message.
+    print(userInfo)
+  }
+
+  // [START receive_message]
+  func application(_ application: UIApplication,
+                   didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
+                     -> Void) {
+    // If you are receiving a notification message while your app is in the background,
+    // this callback will not be fired till the user taps on the notification launching the application.
+    // TODO: Handle data of notification
+
+    // With swizzling disabled you must let Messaging know about the message, for Analytics
+    // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+    // Print message ID.
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
     }
 
+    // Print full message.
+    print(userInfo)
 
+    completionHandler(UIBackgroundFetchResult.newData)
+  }
+
+  // [END receive_message]
+
+  func application(_ application: UIApplication,
+                   didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("Unable to register for remote notifications: \(error.localizedDescription)")
+  }
+
+  // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+  // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+  // the FCM registration token.
+  func application(_ application: UIApplication,
+                   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    print("APNs token retrieved: \(deviceToken.map { String(format: "%02x", $0) }.joined())")
+    
+
+    // With swizzling disabled you must set the APNs token here.
+    // Messaging.messaging().apnsToken = deviceToken
+  }
 }
 
+// [START ios_10_message_handling]
+@available(iOS 10, *)
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  // Receive displayed notifications for iOS 10 devices.
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+                                -> Void) {
+    let userInfo = notification.request.content.userInfo
+
+    // With swizzling disabled you must let Messaging know about the message, for Analytics
+    // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+    // [START_EXCLUDE]
+    // Print message ID.
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
+    }
+    // [END_EXCLUDE]
+
+    // Print full message.
+    print(userInfo)
+
+    // Change this to your preferred presentation option
+    completionHandler([[.banner, .badge, .sound]])
+  }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    let userInfo = response.notification.request.content.userInfo
+
+    // [START_EXCLUDE]
+    // Print message ID.
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
+    }
+    // [END_EXCLUDE]
+
+    // With swizzling disabled you must let Messaging know about the message, for Analytics
+    // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+    // Print full message.
+    print(userInfo)
+
+    completionHandler()
+  }
+}
+
+// [END ios_10_message_handling]
+
+extension AppDelegate: MessagingDelegate {
+  // [START refresh_token]
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
+
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: Notification.Name("FCMToken"),
+      object: nil,
+      userInfo: dataDict
+    )
+      // send token via API
+    let url = URL(string: "https://swclient.site/api/send-device-token")!
+    var request = URLRequest(url: url)
+    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+    request.httpMethod = "POST"
+    let parameters: [String: Any] = [
+      "token": String(describing: fcmToken ?? "")
+    ]
+    request.httpBody = parameters.percentEncoded()
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+    guard let data = data,
+        let response = response as? HTTPURLResponse,
+        error == nil else {                                              // check for fundamental networking error
+        print("error", error ?? "Unknown error")
+        return
+    }
+
+    guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
+        print("statusCode should be 2xx, but is \(response.statusCode)")
+        print("response = \(response)")
+        return
+    }
+
+    let responseString = String(data: data, encoding: .utf8)
+    }
+
+    task.resume()
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
+
+  // [END refresh_token]
+}
+
+extension Dictionary {
+    func percentEncoded() -> Data? {
+        return map { key, value in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+        }
+        .joined(separator: "&")
+        .data(using: .utf8)
+    }
+}
+
+extension CharacterSet {
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+}
--- Smart FBA Client Portal/ViewController.swift
+++ Smart FBA Client Portal/ViewController.swift
@@ -1,19 +1,97 @@
-//
-//  ViewController.swift
-//  Smart FBA Client Portal
-//
-//  Created by Lingga on 09/04/22.
-//
-
 import UIKit
+import WebKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, UNUserNotificationCenterDelegate {
+    
+    let webView = WKWebView()
+    let userNotificationCenter = UNUserNotificationCenter.current()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.userNotificationCenter.delegate = self
+        self.requestNotificationAuthorization()
+        self.sendNotification()
+        
+        webView.frame = view.bounds
+        webView.navigationDelegate = self
+
+        let url = URL(string: "https://swclient.site/mobile")!
+        let urlRequest = URLRequest(url: url)
+
+        webView.load(urlRequest)
+        print(urlRequest)
+        webView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        view.addSubview(webView)
     }
 
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated  {
+            if let url = navigationAction.request.url,
+                let host = url.host, !host.hasPrefix("swclient.site/mobile"),
+                UIApplication.shared.canOpenURL(url) {
+                if (!url.description.contains("swclient.site")) {
+                    UIApplication.shared.open(url)
+                } else {
+                    let urlRequest = URLRequest(url: url)
+                    webView.load(urlRequest)
+                }
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    func requestNotificationAuthorization() {
+            // Code here
+            let authOptions = UNAuthorizationOptions.init(arrayLiteral: .alert, .badge, .sound)
+            self.userNotificationCenter.requestAuthorization(options: authOptions) { (success, error) in
+                if let error = error {
+                    print("Error: ", error)
+                }
+            }
+        }
 
+    func sendNotification() {
+        // Code here
+        let notificationContent = UNMutableNotificationContent()
+
+        // Add the content to the notification content
+        notificationContent.title = "Test"
+        notificationContent.body = "Test body"
+        notificationContent.badge = NSNumber(value: 3)
+
+        // Add an attachment to the notification content
+        if let url = Bundle.main.url(forResource: "dune",
+                                        withExtension: "png") {
+            if let attachment = try? UNNotificationAttachment(identifier: "dune",
+                                                                url: url,
+                                                                options: nil) {
+                notificationContent.attachments = [attachment]
+            }
+        }
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10,
+                                                           repeats: false)
+        let request = UNNotificationRequest(identifier: "testNotification",
+                                           content: notificationContent,
+                                           trigger: trigger)
+       
+        userNotificationCenter.add(request) { (error) in
+           if let error = error {
+               print("Notification Error: ", error)
+           }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
+    }
+    
 }
-
